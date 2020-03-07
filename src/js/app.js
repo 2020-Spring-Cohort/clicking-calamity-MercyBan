a0cce071ef679a74895cd6879859f8a170d590bd
Mercy <mercybaning@gmail.com>
2020-03-07 12:54:20 -0500

    all counts working/multiplier

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,77 @@
+let macaroncount = 0;
+let companionPrice = 100;
+let multiplierPrice = 10;
+let autoClick = 0;
+let amountOfMultipliers = 0;
+let multiplier = 1.0;
+let companionPriceChangeRate = 1.1;
+let multiplierPriceChangeRate = 1.1;
 
-let i = 0
-function addMacaron(){
-    document.querySelectorAll('#macaron')[0].value = ++i;
-    // addMacaron =addMacaron + 1
-    // document.getElementById('text').value = addMacaron;
-    // document.title = addMacaron + "Macaron";
+function addMacaron() {
+    macaroncount += multiplier;
+
+    document.querySelector('#count').innerText = macaroncount.toFixed(0);
+
 }
 
 function getCount() {
-    return i;
+    return macaroncount;
 }
 
-let clickCompanion = 0
-function countClick2(){
-    if(countClick<20){
-        clickCompanion = -1;
-        document.querySelectorAll('macaron').innerHTML = clickCompanion++;
+function timer() {
+    for (let i = 0; i < autoClick; i++) {
+        addMacaron();
     }
-    // function update(){
+}
 
-    // }
-    // let autoClick = 0;
-    // function timer() {
-    //     addMacaron = addMacaron + autoClick;
-    //     update()
-    // }
-    // setInterval(timer, 1000)
+setInterval(timer, 1000)
+
+function reset() {
+    macaroncount = 0;
+    companionPrice = 100;
+    multiplierPrice = 10;
+    autoClick = 0;
+    amountOfMultipliers = 0;
+    multiplier = 1.0;
+    companionPriceChangeRate = 1.1;
+    multiplierPriceChangeRate = 1.1;
+    document.querySelector('#count').innerHTML = macaroncount.toFixed(0);
+    document.querySelector('#multiplier').innerText = multiplier.toFixed(2) + 'x';
+    document.querySelector('#numberOfClickingCompanions').innerText = 'You have ' + autoClick + ' Clicking Companions';
+    document.querySelector('#costOfClickingCompanion').innerText = 'Cost: ' + companionPrice.toFixed(0) + ' Macarons';
+    document.querySelector('#costOfCollectiveCulminationCompounder').innerText = 'Cost: ' + multiplierPrice.toFixed(0) + ' Macarons';
+    document.querySelector('#numberOfCollectiveCulminationCompounders').innerText = 'You own ' + amountOfMultipliers + ' Collective Collective Culmination Compounders';
+};
+
+function purchaseClickingCompanion() {
+    if (macaroncount >= companionPrice) {
+        macaroncount -= companionPrice;
+        autoClick++;
+        companionPrice *= companionPriceChangeRate;
+        companionPriceChangeRate += 0.1;
+        document.querySelector('#numberOfClickingCompanions').innerText = 'You have ' + autoClick + ' Clicking Companions';
+        document.querySelector('#costOfClickingCompanion').innerText = 'Cost: ' + companionPrice.toFixed(0) + ' Macarons';
+    }
+}
+
+function purchaseCollectiveCulminationCompounder() {
+    if (macaroncount >= multiplierPrice) {
+        macaroncount -= multiplierPrice;
+        if (multiplier === 1.0) {
+            multiplier += 0.2;
+        } else {
+            multiplier = Math.pow(1.2, amountOfMultipliers)
+        }
+        amountOfMultipliers++;
+
+        multiplierPrice *= multiplierPriceChangeRate;
+
+        multiplierPriceChangeRate += 0.1;
+
+        document.querySelector('#multiplier').innerText = multiplier.toFixed(2) + 'x';
+
+        document.querySelector('#costOfCollectiveCulminationCompounder').innerText = 'Cost: ' + multiplierPrice.toFixed(0) + ' Macarons';
+
+        document.querySelector('#numberOfCollectiveCulminationCompounders').innerText = 'You own ' + amountOfMultipliers + ' Collective Collective Culmination Compounders'
+    }
 }
